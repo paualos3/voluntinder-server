7d6f6b28548f5d82018943bf3064c89807ffee06
Pau Alos Mairal <paualos3@gmail.com>
2018-03-18 01:58:52 +0100

    fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    mongoDbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/voluntinder',
+    mongoDbUri: process.env.MONGODB_URI || 'mongodb://admin:voluntinder@ds213229.mlab.com:13229/voluntinder',
     enableMorgan: process.env.ENABLE_LOG_MORGAN || false,
 
 }
